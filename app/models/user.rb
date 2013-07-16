0b9c35c333c7ec72052f1fe1f8eaf9f137b3f52f
Matthew Schmaus <mschmaus201@gmail.com>
2013-07-16 18:24:08 -0400

    Move feature list logic in technology show view to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   has_many :features
   has_many :technologies, :through => :features
   has_many :projects, :through => :features
+
+
+  def tech_features(chosen_tech)
+    Feature.joins(:technologies).where(:user_id => self.id, :technologies => {:name => chosen_tech.name})
+  end
+
 end
