efd4a622c92c4aa17f8eed0ab7408cf7d338d99e
Desmond Rawls <captaingrover@gmail.com>
2013-07-20 13:27:06 -0400

    add login/logout to the navbar

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,9 +1,9 @@
 module NavigationHelper
   def login_logout
     if logged_in?
-      "link_to \"Logout\", logout_path"
+      link_to 'Logout', logout_path
     else
-      "link_to \"Login\", login_path"
+      link_to 'Login', login_path
     end
   end
 end
