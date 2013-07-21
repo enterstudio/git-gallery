23034874b7cac07a2d48200a7ace24fe41c3f1ab
Desmond Rawls <captaingrover@gmail.com>
2013-07-21 17:27:36 -0400

    fix portfolio pages so they only display features you made organized by project

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,8 @@ class Project < ActiveRecord::Base
   has_many :users, :through => :features
 
   has_many :uploads, as: :uploadable, :dependent => :destroy
+
+   def this_user_features(chosen_user)
+    Feature.joins(:project).where(:project_id => self.id, :user_id => chosen_user.id)
+  end
 end
