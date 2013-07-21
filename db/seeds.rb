6f3f5a7166862a88e67bb13ce8cb0d0285ece847
Matthew Schmaus <mschmaus201@gmail.com>
2013-07-20 20:39:41 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,47 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-matt = User.create(:name => "Matt")
-matt.features.build(:title => "git-built")
+###################
+# CREATE PROJECTS #
+###################
+git_gallery = Project.create(:name => "Git Gallery")
+recipe_app = Project.create(:name => "Recipe App")
+playlister = Project.create(:name => "Playlister")
+
+################
+# CREATE USERS #
+################
+matt = User.create(:name => "Matt", :email => "matthew.schmaus@flatironschool.com", :password => "password")
+jen = User.create(:name => "Jen", :email => "jen@email.com", :password => "password")
+
+#########################
+# ADD FEATUERS TO USERS #
+#########################
+scraper = Feature.create(:title => "scraper", :user_id => 1, :project_id => 1)
+matt.features.build(:title => "polymorphic associations", :project_id => 1)
+matt.features.build(:title => "playlists", :project_id => 3)
 matt.save
 
-jen = User.create(:name => "Jen")
-jen.features.build(:title => "git-destroy")
+jen.features.build(:title => "git-destroy", :project_id => 1)
+jen.features.build(:title => "git-destroy", :project_id => 2)
 jen.save
 
+#######################
+# CREATE TECHNOLOGIES #
+#######################
+ror = Technology.create(:name => "Ruby on Rails")
+git = Technology.create(:name => "Github")
 js = Technology.create(:name => "Javascript")
-js.features.build(:title => "jquery")
-gd = Feature.where(:title => "git-destroy").first
-js.features.push(gd)
+
+#####################################
+# CONNECT FEATURES AND TECHNOLOGIES #
+#####################################
+js.features.push(Feature.where(:title => "git-destroy").first)
 js.save
 
-
-gitbuilt = Feature.where(:title => "git-built").first
-gitbuilt.technologies.build(:name => "ROR")
+gitbuilt = Feature.where(:title => "playlists").first
+gitbuilt.technologies.build(:name => "Ruby on Rails")
 gitbuilt.save
 
+# Will this work? Feature not associated to a user
+js.features.build(:title => "jquery")
