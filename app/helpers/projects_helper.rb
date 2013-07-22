eaaab2d258f35347d7b1f7ef746a0b61cb132f38
Desmond Rawls <captaingrover@gmail.com>
2013-07-22 17:39:56 -0400

    update projects thumbnail grid on main page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,11 @@
 module ProjectsHelper
-
+	def link_to_project_image(project)
+		if project.uploads.count > 0
+			link_to image_tag(project.uploads.first.image_url.to_s), project_path(project)
+		else
+			link_to image_tag("http://octodex.github.com/images/octocat-de-los-muertos.jpg"), project_path(project)
+		end
+	end
   # def edit_project_button
   #   if current_user_can?(:edit, @project)
   #     "<button>Edit Project</button>"
