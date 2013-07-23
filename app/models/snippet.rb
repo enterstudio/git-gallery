2ba24d357339e4e22a32116c716a5d6582794644
micahrcorn <micahc126@gmail.com>
2013-07-23 12:41:08 -0400

    edit the new feature form to show snippet description

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,5 +1,6 @@
 class Snippet < ActiveRecord::Base
-  attr_accessible :code, :language, :name, :feature_id, :url, :description
 
+  attr_accessible :code, :language, :name, :feature_id, :url, :description
   belongs_to :feature
+  
 end
