7ac7808f2c5b3678a2519d5adc5cc4b2a012586e
Matthew Schmaus <mschmaus201@gmail.com>
2013-07-24 16:34:19 -0400

    Extensive CSS work incorporating dashboard & bootstrap on user and project index

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
       user.avatar_url
     else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=72"
     end
   end
 end
