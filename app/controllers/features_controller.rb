cb9aefd0898f3d2f9e71778c89bea7091fadea62
jmlcampbell <jennifercampbell_9@hotmail.com>
2013-07-30 14:44:38 -0400

    slide ordering using drag and drop

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -62,6 +62,11 @@ class FeaturesController < ApplicationController
   # PUT /users/1/features/1
   # PUT /users/1/features/1.json
   def update
+    params[:slides].each_with_index do |slide, index|
+      snippet_or_upload = slide[:class].constantize.find(slide[:id])
+      snippet_or_upload.position = index + 1
+      snippet_or_upload.save
+    end
     params[:feature][:technology_ids] ||= []
     @feature = Feature.find(params[:id])
 
