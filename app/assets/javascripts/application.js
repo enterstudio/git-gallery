e4b2af3dc144e6c7a7bcfbb3e365e312cd3b2909
jmlcampbell <jennifercampbell_9@hotmail.com>
2013-07-31 15:05:18 -0400

    fixed technologies button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,27 +31,6 @@ $(function(){
 	$('.sortable').sortable().bind('sortupdate', function() {
 	});
 
-
-	$('.carousel').carousel();
-
-	$('.carousel').each(function() {
-	  $(this).find('.item:first').addClass('active');
-	});
-
-	$('.item1').hide();
-
-	$('.item1:first').show();
-	$('.feature').eq(0).addClass('selected');
-
-	$('.feature h2').click(function() {
-	  $('.feature').removeClass('selected');
-	  $(this).parent().addClass('selected');
-
-	  $('.item1').hide();
-	  var sel = "#"+$(this).data('name');
-	  $(sel).show();
-	});
-
 });
 
 
