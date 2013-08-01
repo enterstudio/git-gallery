673679091c3dde81682005c91a79519fb49e5502
Matthew Schmaus <mschmaus201@gmail.com>
2013-08-01 12:02:22 -0400

    Destroy session before destorying user and redirect to root if user tries to delete account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,6 +83,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    session[:user_id] = nil
     @user = User.find(params[:id])
     @user.destroy
 
--- app/models/repo_scraper.rb
+++ app/models/repo_scraper.rb
@@ -12,7 +12,6 @@ class RepoScraper
     repos = JSON.parse(open("https://api.github.com/users/#{@user.name}/repos?access_token=#{@user.token}").read)
 
     repos.each do |repo|
-      
       if Repo.where(:github_id => repo["id"]).empty?
         new_repo = Repo.new
         new_repo.github_id = repo["id"]
@@ -20,13 +19,10 @@ class RepoScraper
         new_repo.published_at = repo["published_at"]
         new_repo.url = repo["url"]
         new_repo.html_url = repo["html_url"]
-        new_repo.github_id = repo["id"]
         new_repo.user_id = @user.id
 
         puts "saved successfully" if new_repo.save
       end
     end     
   end
-
 end
-
