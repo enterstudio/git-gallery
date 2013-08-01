07d39f94e78217244f06c5e3f8e4eab99b233ceb
Desmond Rawls <captaingrover@gmail.com>
2013-08-01 15:16:51 -0400

    add instructions to new_slide_prompt partial

--- app/helpers/slides_helper.rb
+++ app/helpers/slides_helper.rb
@@ -1,7 +1,7 @@
 module SlidesHelper
   def slide_prompt(feature)
     if feature.uploads.count == 0 && feature.snippets.count == 0
-      "What would you like on your first slide?" 
+      render :partial => 'new_slide_prompt'
     else
       "What would you like on your next slide?"
     end
