6a75372d96cbbc69fcfe100aab946980c3ab26a8
Matthew Schmaus <mschmaus201@gmail.com>
2013-08-01 17:47:39 -0400

    Rename uid column in users to github_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :technologies, :features
+  attr_accessible :email, :name, :github_id
 
   has_many :repos
   
@@ -47,7 +47,7 @@ class User < ActiveRecord::Base
   def self.create_from_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
-      user.uid = auth["uid"]
+      user.github_id = auth["uid"]
       user.name = auth["info"]["nickname"]
       user.email = auth["info"]["email"]
       user.avatar_url = auth["info"]["image"]
