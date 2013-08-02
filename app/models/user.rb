f89223b4d2d7bc1e77f8d740e9bd852dc6f75603
Matthew Schmaus <mschmaus201@gmail.com>
2013-08-01 22:04:29 -0400

    Fix column name reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+    where(auth.slice("provider", "github_id")).first || create_from_omniauth(auth)
   end
 
   def self.create_from_omniauth(auth)
