8647a7efc206bc8d769ba3224da9c34b5ca69ea4
Matthew Schmaus <mschmaus201@gmail.com>
2013-08-03 15:42:28 -0400

    Fix config call issue in setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
@@ -9,5 +9,5 @@ config.action_mailer.smtp_settings = {
   :enable_starttls_auto => true
 }
 
-# ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+# ActionMailer::Base.default_url_options[:host] = "localhost:3000"  #Moved to config/application.rb
 # Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
