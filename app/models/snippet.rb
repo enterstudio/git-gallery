549fd8383469c306a8c3be26c6c903abd2864ae2
Desmond Rawls <captaingrover@gmail.com>
2013-08-03 18:05:05 -0400

    put error messages in a shared partial. add a description validation for snippet with issues on render :new

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,6 +1,8 @@
 class Snippet < ActiveRecord::Base
-
   attr_accessible :code, :language, :name, :feature_id, :url, :description, :position
+  
   belongs_to :feature
+
+  validates_presence_of :description, :message => ("Please fill in a description.")
   
 end
