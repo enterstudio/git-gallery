a674f47885c64cedc9f2922bdf8f1e6ec65b594d
micahrcorn <micahc126@gmail.com>
2013-08-07 17:26:43 -0400

    create conditional validation for upload description

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,13 +2,13 @@ class Upload < ActiveRecord::Base
   attr_accessible :extension, :name, :image, :description, :position
   belongs_to :uploadable, polymorphic: true
 
-  validates_presence_of :description, :if => :uploadable_is_feature?
+  
 
   mount_uploader :image, ImageUploader
 
-  validates_presence_of :image
+  validates_presence_of :description, :if => :uploadable_is_feature?
+  # validates_presence_of :image
   validates_presence_of :name
-  validates_presence_of :description
 
   def shift_position(index)
     self.position = index.to_i + 1
