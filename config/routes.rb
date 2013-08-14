db16058f3685922149241e9865a51143ddca1380
jmlcampbell <jennifercampbell_9@hotmail.com>
2013-08-14 12:37:48 -0400

    nav bar added

--- config/routes.rb
+++ config/routes.rb
@@ -80,7 +80,7 @@ GitAtMe::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'projects#landing'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
