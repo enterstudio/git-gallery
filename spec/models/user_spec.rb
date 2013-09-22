97c5ea9557eb35b1ae77e07caee9f38b5c6b8988
Desmond Rawls <captaingrover@gmail.com>
2013-09-22 15:15:25 -0400

    add a validations test suite for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,20 @@ describe User do
     it { should have_many(:technologies) }
   end
 
+  describe 'validations' do
+    it { should validate_uniqueness_of(:email) }
+    it 'should allow a properly formatted email' do
+      user = User.create(:email => 'example@example.com')
+      user.should be_valid
+    end
+    it 'should not allow an improperly formatted email' do
+      user = User.create(:email => 'grandmasayshello')
+      user.should_not be_valid
+    end
+  end
+
 end
+
+ 
+
+ 
