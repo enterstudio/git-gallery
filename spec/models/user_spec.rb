5a0c35a01394f166675250fffa2f427b8035c283
Desmond Rawls <captaingrover@gmail.com>
2013-09-22 15:00:57 -0400

    add an associations test to the users model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe User do
 
+  describe 'associations' do
+    it { should have_many(:repos) }
+    it { should have_many(:user_projects) }
+    it { should have_many(:projects) }
+    it { should have_many(:technologies) }
+  end
+
 end
